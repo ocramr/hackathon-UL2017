00e3f2917ccb59eaf94b082165c507a1a82e3c61
Ocramr <14249346+ocramr@users.noreply.github.com>
2017-03-10 15:57:36 +0100

    Controller

---
+++ backend/src/controller/GameController.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+class GameController extends AbstractController
+{
+
+    public function saveGame(Request $request, Response $response, $args){
+
+    }
+
+}
