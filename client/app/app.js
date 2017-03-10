c6a51bef5494e6460468f0d0fee8731e20165ab4
Ocramr <14249346+ocramr@users.noreply.github.com>
2017-03-10 17:35:42 +0100

    Added routes

--- client/app/app.js
+++ client/app/app.js
@@ -1,9 +1,20 @@
-var app = angular.module('spotyGame', ['spotify']);
+var app = angular.module('spotyGame', ['spotify', 'ngRoute']);
 
-app.config(function($logProvider, SpotifyProvider){
-  $logProvider.debugEnabled(true);
+app.config(function($routeProvider, $locationProvider, $logProvider, SpotifyProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: '../assets/templates/play.html',
+            controller: 'gameController'
+        })
+        .when('/play', {
+            templateUrl: '../assets/templates/game.html',
+            controller: 'gameController'
+        }).otherwise({redirectTo: '/sdsd'}
+    );
+
+    $logProvider.debugEnabled(true);
   SpotifyProvider.setClientId('e3cd640ff09c48789ea6708e8187cee0');
-  SpotifyProvider.setRedirectUri('http://localhost/hackathon-UL2017/client/');
+  SpotifyProvider.setRedirectUri('http://spotyplay.local/play');
   SpotifyProvider.setScope('playlist-read-private');
 });
     
