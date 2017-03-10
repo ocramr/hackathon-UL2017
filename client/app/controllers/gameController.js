ee3cf3483d2d8e5b08776947cfae348a37a7611a
ahmani <ikram.ahmani@gmail.com>
2017-03-10 20:20:09 +0100

    get songs

--- client/app/controllers/gameController.js
+++ client/app/controllers/gameController.js
@@ -1,5 +1,8 @@
 angular.module('spotyGame').controller('gameController', ["$scope" ,"Spotify", "$location" ,function ($scope, Spotify) {
 
+    $scope.position = 0;
+    $scope.songs= [];
+
     var getSongs = function() {
 
         $getRandomOffset = Math.floor((Math.random() * 1000) + 1);
@@ -9,12 +12,16 @@ angular.module('spotyGame').controller('gameController', ["$scope" ,"Spotify", "
         };
         Spotify.search('genre:pop', 'track', $options).then(function (response) {
             $scope.songs = response.data.tracks.items;
+            $scope.song = $scope.songs[$scope.position];    
+            
         });
     };
+
 
     var initGame = function () {
         Spotify.getCategories({}).then(function (response) {
             $scope.categories = response.data.categories.items;
+            console.log($scope.categories)
         });
 
         getSongs();
