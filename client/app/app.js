2f09eea565bc6448c527d843d047cb72bf705696
Ismail Daoudy <Ismail Daoudy>
2017-03-10 17:41:46 +0100

    CLEAN HTML

--- client/app/app.js
+++ client/app/app.js
@@ -3,7 +3,7 @@ var app = angular.module('spotyGame', ['spotify']);
 app.config(function($logProvider, SpotifyProvider){
   $logProvider.debugEnabled(true);
   SpotifyProvider.setClientId('e3cd640ff09c48789ea6708e8187cee0');
-  SpotifyProvider.setRedirectUri('http://localhost/hackathon-UL2017/client/');
+  SpotifyProvider.setRedirectUri('http://spotyplay.local/play');
   SpotifyProvider.setScope('playlist-read-private');
 });
     
