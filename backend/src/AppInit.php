406a7383ad6e8ae36d49f6b83fa67e395e871b57
Ocramr <14249346+ocramr@users.noreply.github.com>
2017-03-10 13:45:41 +0100

    First commit

---
+++ backend/src/AppInit.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Manager;
+
+class AppInit
+{
+    public static function bootEloquent($file){
+        $conf = parse_ini_file($file);
+        $db = new Manager();
+        $db->addConnection($conf);
+        $db->setAsGlobal();
+        $db->bootEloquent();
+        Manager::connection()->enableQueryLog();
+    }
+}
---
+++ backend/src/controller/AbstractController.php
@@ -0,0 +1,17 @@
+<?php
+
+use Interop\Container\ContainerInterface;
+
+class AbstractController
+{
+    protected $container;
+
+    /**
+     * APIController constructor.
+     * @param $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+}
