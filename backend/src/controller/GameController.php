1675dd213b88b6cceba9c5e9a42f41601708a70e
Ocramr <14249346+ocramr@users.noreply.github.com>
2017-03-11 03:40:47 +0100

    bd changes

--- backend/src/controller/GameController.php
+++ backend/src/controller/GameController.php
@@ -24,31 +24,33 @@ class GameController extends AbstractController
     		if (!isset($data["gameName"]) || !isset($data["userName"]) || !isset($data["owner"]) || !isset($data["songs"]))
     			return $this->json_error($response, 400, "Missing parameters");
 
-    		$player = Player::where("id", "=", filter_var($data['owner'], FILTER_SANITIZE_STRING))->first();
-            if (empty($player)) {
+    		$player = Player::where("spotify_id", "=", filter_var($data['owner'], FILTER_SANITIZE_STRING))->first();
+            if (is_null($player)) {
                 $player = new Player();
-        		$player->id = filter_var($data['owner'], FILTER_SANITIZE_STRING);
+        		$player->spotify_id = filter_var($data['owner'], FILTER_SANITIZE_STRING);
         		$player->pseudo = filter_var($data['userName'], FILTER_SANITIZE_STRING);
         		$player->save();
             }
-
             $game = new Game();
             $game->name = filter_var($data['gameName'], FILTER_SANITIZE_STRING);
-            $game->owner = filter_var($data['owner'], FILTER_SANITIZE_STRING);
+            $game->owner = $player->spotify_id;
             $game->save();
 
     		foreach ($data["songs"] as $song) {
-    			$newSong = new Song();
-    			$newSong->id = filter_var($song['id'], FILTER_SANITIZE_STRING);
-    			$newSong->name = filter_var($song['name'], FILTER_SANITIZE_STRING);
-    			$newSong->url = filter_var($song['uri'], FILTER_SANITIZE_STRING);
-    			$newSong->preview_url = filter_var($song['preview_url'], FILTER_SANITIZE_STRING);
-    			$newSong->save();
-
-    			$game_song = new Game_Song();
+    		    $newSong = Song::find($song['id']);
+    		    if(is_null($newSong)){
+                    $newSong = new Song();
+                    $newSong->id = filter_var($song['id'], FILTER_SANITIZE_STRING);
+                    $newSong->name = filter_var($song['name'], FILTER_SANITIZE_STRING);
+                    $newSong->url = filter_var($song['uri'], FILTER_SANITIZE_STRING);
+                    $newSong->preview_url = filter_var($song['preview_url'], FILTER_SANITIZE_STRING);
+                    $newSong->save();
+                }
+                $newSong->games()->attach($game);
+    			/*$game_song = new Game_Song();
     			$game_song->id_game = $game->id;
-    			$game_song->id_song = filter_var($song['id'], FILTER_SANITIZE_STRING);
-    			$game_song->save();
+    			$game_song->id_song = $newSong->id;*/
+    			$newSong->save();
     		}
 
     		$game_player = new Game_Player();
@@ -87,7 +89,7 @@ class GameController extends AbstractController
             $id = filter_var($args['id']);
             $data = $request->getParsedBody();
 
-            $player = Player::where("id", "=", filter_var($data['player_id'], FILTER_SANITIZE_STRING))->first();
+            $player = Player::where("spotify_id", "=", filter_var($data['owner'], FILTER_SANITIZE_STRING))->first();
             if (empty($player)) {
                 $player = new Player();
                 $player->id = filter_var($data['owner'], FILTER_SANITIZE_STRING);
