dada1326f07976eeeb087338e753369d5680adc8
ahmani <ikram.ahmani@gmail.com>
2017-03-11 02:19:04 +0100

    merge

--- client/app/factory/gameFactory.js
+++ client/app/factory/gameFactory.js
@@ -1,8 +1,8 @@
-angular.module('spotyGame').factory('GameFactory', ['BACKEND_URL','$http',function (API_URL, $http) {
+angular.module('spotyGame').factory('GameFactory', ['BACKEND_URL','$http',function (BACKEND_URL, $http) {
 
     return {
         startGame:function (json) {
-            return $http.post(API_URL+'game/play', json);
+            return $http.post(BACKEND_URL+'game/play', json);
         }
     }
 
